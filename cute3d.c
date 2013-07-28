7487bba2eec714d6a596314ed5e4b23b42f3e2ac
Andreas Raster <lazor@affenbande.org>
2013-07-28 02:33:52 +0200

    simple fps counting

--- cute3d.c
+++ cute3d.c
@@ -205,6 +205,10 @@ int main(int argc, char** argv) {
     struct Font* foo = font_allocate_ascii("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz", symbols);
     int font_id = font_registry(NewFont, 0, &foo);
 
+    double fps = 0;
+    int frames_done = 0;
+    double old_time = al_get_time();
+
     while (true) {
         /* Check for ESC key or close button event and quit in either case. */
         if (!al_is_event_queue_empty(queue)) {
@@ -279,6 +283,16 @@ int main(int argc, char** argv) {
         }
 
         al_flip_display();
+
+        double game_time = al_get_time();
+        if( game_time - old_time >= 1.0 ) {
+            fps = frames_done / (game_time - old_time);
+            frames_done = 0;
+            old_time = game_time;
+        }
+        frames_done++;
+
+        printf("%f\n", fps);
     }
 
 done:
