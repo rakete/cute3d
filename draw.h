1c8ac2ce9ffd0f4058b6011cc67e891f333844dc
Andreas Raster <lazor@affenbande.org>
2013-12-06 01:25:05 +0100

    fix indentation

--- draw.h
+++ draw.h
@@ -30,31 +30,31 @@
 #include "glsl.h"
 
 void draw_grid(int instances,
-                int steps,
-                float color[4],
-                float projection_matrix[16],
-                float view_matrix[16],
-                float model_matrix[][16]);
+               int steps,
+               float color[4],
+               float projection_matrix[16],
+               float view_matrix[16],
+               float model_matrix[][16]);
 
 void draw_normals_array(float* vertices,
-                         float* normals,
+                        float* normals,
+                        int n,
+                        float color[4],
+                        float projection_matrix[16],
+                        float view_matrix[16],
+                        float model_matrix[16]);
+
+void draw_normals_buffer(GLuint vertices,
+                         GLuint normals,
                          int n,
                          float color[4],
                          float projection_matrix[16],
                          float view_matrix[16],
                          float model_matrix[16]);
 
-void draw_normals_buffer(GLuint vertices,
-                          GLuint normals,
-                          int n,
-                          float color[4],
-                          float projection_matrix[16],
-                          float view_matrix[16],
-                          float model_matrix[16]);
-
 void draw_texture_quad(GLuint texture_id,
-                        float projection_matrix[16],
-                        float view_matrix[16],
-                        float model_matrix[16]);
+                       float projection_matrix[16],
+                       float view_matrix[16],
+                       float model_matrix[16]);
 
 #endif
