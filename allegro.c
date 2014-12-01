57d4a3f4320677f1a5adf334551d164c8bcd0059
Andreas Raster <lazor@affenbande.org>
2014-12-01 01:59:42 +0100

    enable blending in default allegro context

--- allegro.c
+++ allegro.c
@@ -17,6 +17,9 @@ void allegro_display(int width, int height, ALLEGRO_DISPLAY** display) {
     glDepthFunc(GL_LESS);
     glEnable(GL_DEPTH_TEST);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     init_shader();
 }
 
