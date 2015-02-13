187c9e1e8f587f7dc3b64440da3a24482826b7be
Andreas Raster <lazor@affenbande.org>
2015-02-13 22:20:04 +0100

    introduce pointer types in matrix.h
    - add vector_copy, matrix_copy functions
    - add vadd, vmul op
    - fix vcross, mmul op overwrites arg
    - rename vector_length -> vector_magnitude
    - add matrix_transpose

--- matrix.c
+++ matrix.c
@@ -16,6 +16,13 @@
 
 #include "matrix.h"
 
+void vector_copy(const Vec v, Vec r) {
+    r[0] = v[0];
+    r[1] = v[1];
+    r[2] = v[2];
+    r[3] = v[3];
+}
+
 void vector_add(const Vec v, const Vec w, Vec r) {
     r[0] = v[0] + w[0];
     r[1] = v[1] + w[1];
@@ -26,6 +33,11 @@ void vector_add(const Vec v, const Vec w, Vec r) {
     } else {
         r[3] = w3;
     }
+}
+
+VecP vadd(const Vec v, Vec w) {
+    vector_add(v,w,w);
+    return w;
 }
 
 void vector_add3f(const Vec v, const Vec3f w, Vec r) {
@@ -57,16 +69,28 @@ void vector_multiply(const Vec v, const Vec w, Matrix m) {
     printf("4f vector_multiply not implemented yet!");
 }
 
-void vector_multiply1f(const Vec v, float w, Vec r) {
-    r[0] = v[0]*w;
-    r[1] = v[1]*w;
-    r[2] = v[2]*w;
-    r[3] = v[3]*w;
+VecP vmul(Vec v, Matrix w) {
+    Vec t;
+    t[0] = w[0];
+    t[1] = w[1];
+    t[2] = w[2];
+    t[3] = w[3];
+
+    vector_multiply(v,t,w);
+    return w;
 }
 
-float* vmul1f(Vec v, float w) {
-    vector_multiply1f(v,w,v);
-    return v;
+
+void vector_multiply1f(float v, const Vec w, Vec r) {
+    r[0] = v*w[0];
+    r[1] = v*w[1];
+    r[2] = v*w[2];
+    r[3] = v*w[3];
+}
+
+VecP vmul1f(float v, Vec w) {
+    vector_multiply1f(v,w,w);
+    return w;
 }
 
 void vector_invert(const Vec v, Vec r) {
@@ -75,7 +99,7 @@ void vector_invert(const Vec v, Vec r) {
     r[2] = -v[2];
 }
 
-float* vinv(Vec v) {
+VecP vinv(Vec v) {
     vector_invert(v,v);
     return v;
 }
@@ -92,41 +116,44 @@ float vdot(const Vec v, const Vec w) {
 }
 
 void vector_cross(const Vec v, const Vec w, Vec r) {
-    r[0] = v[1]*w[2] - v[2]*w[1];
-    r[1] = v[2]*w[0] - v[0]*w[2]; 
-    r[2] = v[0]*w[1] - v[1]*w[0]; 
+    Vec t;
+    t[0] = v[1]*w[2] - v[2]*w[1];
+    t[1] = v[2]*w[0] - v[0]*w[2];
+    t[2] = v[0]*w[1] - v[1]*w[0];
+
+    r[0] = t[0]; r[1] = t[1]; r[2] = t[2];
 }
 
-float* vcross(const Vec v, Vec w) {
+VecP vcross(const Vec v, Vec w) {
     vector_cross(v,w,w);
     return w;
 }
 
-void vector_length(const Vec v, float* r) {
+void vector_magnitude(const Vec v, VecP r) {
     *r = sqrt( v[0]*v[0] + v[1]*v[1] + v[2]*v[2] );
 }
 
-float vlength(const Vec v) {
+float vmagnitude(const Vec v) {
     float r;
-    vector_length(v,&r);
+    vector_magnitude(v,&r);
     return r;
 }
 
 void vector_normalize(const Vec v, Vec r) {
-    float norm = vlength(v);
+    float norm = vmagnitude(v);
     r[0] = v[0] / norm;
     r[1] = v[1] / norm;
     r[2] = v[2] / norm;
     r[3] = 1.0;
 }
 
-float* vnormalize(Vec v) {
+VecP vnormalize(Vec v) {
     vector_normalize(v,v);
     return v;
 }
 
-void vector_angle(const Vec v, const Vec w, float* r) {
-    *r = acos(vdot(v,w) / (vlength(v) * vlength(w)));
+void vector_angle(const Vec v, const Vec w, VecP r) {
+    *r = acos(vdot(v,w) / (vmagnitude(v) * vmagnitude(w)));
 }
 
 float vangle(const Vec v, const Vec w) {
@@ -141,8 +168,9 @@ void vector_isnull(const Vec v, bool* r) {
         v[2] == 0.0 )
     {
         *r = 1;
+    } else {
+        *r = 0;
     }
-    r = 0;
 }
 
 bool vnullp(const Vec v) {
@@ -156,6 +184,13 @@ void vector_perpendicular(const Vec v, Vec r) {
     r[1] = -v[1];
     r[2] = -v[0];
     r[3] = v[3];
+}
+
+void matrix_copy(const Matrix m, Matrix r) {
+    r[0] = m[0];  r[4] = m[4];  r[8] = m[8];   r[12] = m[12];
+    r[1] = m[1];  r[5] = m[5];  r[9] = m[9];   r[13] = m[13];
+    r[2] = m[2];  r[6] = m[6];  r[10] = m[10]; r[14] = m[14];
+    r[3] = m[3];  r[7] = m[7];  r[11] = m[11]; r[15] = m[15];
 }
 
 void matrix_perspective(float left, float right, float top, float bottom, float zNear, float zFar, Matrix m) {
@@ -191,130 +226,130 @@ void matrix_invert(const Matrix m, Matrix r, double* det) {
     int i;
 
     inv[0] =
-        m[5]  * m[10] * m[15] - 
-        m[5]  * m[11] * m[14] - 
-        m[9]  * m[6]  * m[15] + 
+        m[5]  * m[10] * m[15] -
+        m[5]  * m[11] * m[14] -
+        m[9]  * m[6]  * m[15] +
         m[9]  * m[7]  * m[14] +
-        m[13] * m[6]  * m[11] - 
+        m[13] * m[6]  * m[11] -
         m[13] * m[7]  * m[10];
 
     inv[4] =
-        -m[4] * m[10] * m[15] + 
-        m[4]  * m[11] * m[14] + 
-        m[8]  * m[6]  * m[15] - 
-        m[8]  * m[7]  * m[14] - 
-        m[12] * m[6]  * m[11] + 
+        -m[4] * m[10] * m[15] +
+        m[4]  * m[11] * m[14] +
+        m[8]  * m[6]  * m[15] -
+        m[8]  * m[7]  * m[14] -
+        m[12] * m[6]  * m[11] +
         m[12] * m[7]  * m[10];
 
     inv[8] =
-        m[4]  * m[9] * m[15] - 
-        m[4]  * m[11] * m[13] - 
-        m[8]  * m[5] * m[15] + 
-        m[8]  * m[7] * m[13] + 
-        m[12] * m[5] * m[11] - 
+        m[4]  * m[9] * m[15] -
+        m[4]  * m[11] * m[13] -
+        m[8]  * m[5] * m[15] +
+        m[8]  * m[7] * m[13] +
+        m[12] * m[5] * m[11] -
         m[12] * m[7] * m[9];
 
     inv[12] =
-        -m[4] * m[9] * m[14] + 
+        -m[4] * m[9] * m[14] +
         m[4]  * m[10] * m[13] +
-        m[8]  * m[5] * m[14] - 
-        m[8]  * m[6] * m[13] - 
-        m[12] * m[5] * m[10] + 
+        m[8]  * m[5] * m[14] -
+        m[8]  * m[6] * m[13] -
+        m[12] * m[5] * m[10] +
         m[12] * m[6] * m[9];
 
     inv[1] =
-        -m[1] * m[10] * m[15] + 
-        m[1]  * m[11] * m[14] + 
-        m[9]  * m[2] * m[15] - 
-        m[9]  * m[3] * m[14] - 
-        m[13] * m[2] * m[11] + 
+        -m[1] * m[10] * m[15] +
+        m[1]  * m[11] * m[14] +
+        m[9]  * m[2] * m[15] -
+        m[9]  * m[3] * m[14] -
+        m[13] * m[2] * m[11] +
         m[13] * m[3] * m[10];
 
     inv[5] =
-        m[0]  * m[10] * m[15] - 
-        m[0]  * m[11] * m[14] - 
-        m[8]  * m[2] * m[15] + 
-        m[8]  * m[3] * m[14] + 
-        m[12] * m[2] * m[11] - 
+        m[0]  * m[10] * m[15] -
+        m[0]  * m[11] * m[14] -
+        m[8]  * m[2] * m[15] +
+        m[8]  * m[3] * m[14] +
+        m[12] * m[2] * m[11] -
         m[12] * m[3] * m[10];
 
     inv[9] =
-        -m[0] * m[9] * m[15] + 
-        m[0]  * m[11] * m[13] + 
-        m[8]  * m[1] * m[15] - 
-        m[8]  * m[3] * m[13] - 
-        m[12] * m[1] * m[11] + 
+        -m[0] * m[9] * m[15] +
+        m[0]  * m[11] * m[13] +
+        m[8]  * m[1] * m[15] -
+        m[8]  * m[3] * m[13] -
+        m[12] * m[1] * m[11] +
         m[12] * m[3] * m[9];
 
     inv[13] =
-        m[0]  * m[9] * m[14] - 
-        m[0]  * m[10] * m[13] - 
-        m[8]  * m[1] * m[14] + 
-        m[8]  * m[2] * m[13] + 
-        m[12] * m[1] * m[10] - 
+        m[0]  * m[9] * m[14] -
+        m[0]  * m[10] * m[13] -
+        m[8]  * m[1] * m[14] +
+        m[8]  * m[2] * m[13] +
+        m[12] * m[1] * m[10] -
         m[12] * m[2] * m[9];
 
-    inv[2] = m[1]  * m[6] * m[15] - 
-        m[1]  * m[7] * m[14] - 
-        m[5]  * m[2] * m[15] + 
-        m[5]  * m[3] * m[14] + 
-        m[13] * m[2] * m[7] - 
+    inv[2] = m[1]  * m[6] * m[15] -
+        m[1]  * m[7] * m[14] -
+        m[5]  * m[2] * m[15] +
+        m[5]  * m[3] * m[14] +
+        m[13] * m[2] * m[7] -
         m[13] * m[3] * m[6];
 
     inv[6] =
-        -m[0] * m[6] * m[15] + 
-        m[0]  * m[7] * m[14] + 
-        m[4]  * m[2] * m[15] - 
-        m[4]  * m[3] * m[14] - 
-        m[12] * m[2] * m[7] + 
+        -m[0] * m[6] * m[15] +
+        m[0]  * m[7] * m[14] +
+        m[4]  * m[2] * m[15] -
+        m[4]  * m[3] * m[14] -
+        m[12] * m[2] * m[7] +
         m[12] * m[3] * m[6];
 
     inv[10] =
-        m[0]  * m[5] * m[15] - 
-        m[0]  * m[7] * m[13] - 
-        m[4]  * m[1] * m[15] + 
-        m[4]  * m[3] * m[13] + 
-        m[12] * m[1] * m[7] - 
+        m[0]  * m[5] * m[15] -
+        m[0]  * m[7] * m[13] -
+        m[4]  * m[1] * m[15] +
+        m[4]  * m[3] * m[13] +
+        m[12] * m[1] * m[7] -
         m[12] * m[3] * m[5];
 
     inv[14] =
-        -m[0] * m[5] * m[14] + 
-        m[0]  * m[6] * m[13] + 
-        m[4]  * m[1] * m[14] - 
-        m[4]  * m[2] * m[13] - 
-        m[12] * m[1] * m[6] + 
+        -m[0] * m[5] * m[14] +
+        m[0]  * m[6] * m[13] +
+        m[4]  * m[1] * m[14] -
+        m[4]  * m[2] * m[13] -
+        m[12] * m[1] * m[6] +
         m[12] * m[2] * m[5];
 
     inv[3] =
-        -m[1] * m[6] * m[11] + 
-        m[1]  * m[7] * m[10] + 
-        m[5]  * m[2] * m[11] - 
-        m[5]  * m[3] * m[10] - 
-        m[9]  * m[2] * m[7] + 
+        -m[1] * m[6] * m[11] +
+        m[1]  * m[7] * m[10] +
+        m[5]  * m[2] * m[11] -
+        m[5]  * m[3] * m[10] -
+        m[9]  * m[2] * m[7] +
         m[9]  * m[3] * m[6];
 
     inv[7] =
-        m[0] * m[6] * m[11] - 
-        m[0] * m[7] * m[10] - 
-        m[4] * m[2] * m[11] + 
-        m[4] * m[3] * m[10] + 
-        m[8] * m[2] * m[7] - 
+        m[0] * m[6] * m[11] -
+        m[0] * m[7] * m[10] -
+        m[4] * m[2] * m[11] +
+        m[4] * m[3] * m[10] +
+        m[8] * m[2] * m[7] -
         m[8] * m[3] * m[6];
 
     inv[11] =
-        -m[0] * m[5] * m[11] + 
-        m[0]  * m[7] * m[9] + 
-        m[4]  * m[1] * m[11] - 
-        m[4]  * m[3] * m[9] - 
-        m[8]  * m[1] * m[7] + 
+        -m[0] * m[5] * m[11] +
+        m[0]  * m[7] * m[9] +
+        m[4]  * m[1] * m[11] -
+        m[4]  * m[3] * m[9] -
+        m[8]  * m[1] * m[7] +
         m[8]  * m[3] * m[5];
 
     inv[15] =
-        m[0] * m[5] * m[10] - 
-        m[0] * m[6] * m[9] - 
-        m[4] * m[1] * m[10] + 
-        m[4] * m[2] * m[9] + 
-        m[8] * m[1] * m[6] - 
+        m[0] * m[5] * m[10] -
+        m[0] * m[6] * m[9] -
+        m[4] * m[1] * m[10] +
+        m[4] * m[2] * m[9] +
+        m[8] * m[1] * m[6] -
         m[8] * m[2] * m[5];
 
     double d;
@@ -331,38 +366,41 @@ void matrix_invert(const Matrix m, Matrix r, double* det) {
     if(det) *det = d;
 }
 
-float* minv(Matrix m, double* det) {
+MatrixP minv(Matrix m, double* det) {
     matrix_invert(m,m,det);
     return m;
 }
 
 void matrix_multiply(const Matrix m, const Matrix n, Matrix r) {
-    Matrix temp;
+    Matrix t;
     for(int i = 0; i < 4; i++){
-        temp[i*4]   = m[i*4] * n[0] + m[i*4+1] * n[4] + m[i*4+2] * n[8]  + m[i*4+3] * n[12];
-        temp[i*4+1] = m[i*4] * n[1] + m[i*4+1] * n[5] + m[i*4+2] * n[9]  + m[i*4+3] * n[13];
-        temp[i*4+2] = m[i*4] * n[2] + m[i*4+1] * n[6] + m[i*4+2] * n[10] + m[i*4+3] * n[14];
-        temp[i*4+3] = m[i*4] * n[3] + m[i*4+1] * n[7] + m[i*4+2] * n[11] + m[i*4+3] * n[15];
+        t[i*4]   = m[i*4] * n[0] + m[i*4+1] * n[4] + m[i*4+2] * n[8]  + m[i*4+3] * n[12];
+        t[i*4+1] = m[i*4] * n[1] + m[i*4+1] * n[5] + m[i*4+2] * n[9]  + m[i*4+3] * n[13];
+        t[i*4+2] = m[i*4] * n[2] + m[i*4+1] * n[6] + m[i*4+2] * n[10] + m[i*4+3] * n[14];
+        t[i*4+3] = m[i*4] * n[3] + m[i*4+1] * n[7] + m[i*4+2] * n[11] + m[i*4+3] * n[15];
     }
 
     for(int i = 0; i < 16; i++ ) {
-        r[i] = temp[i];
+        r[i] = t[i];
     }
 }
 
-float* mmul(const Matrix m, Matrix n) {
+MatrixP mmul(const Matrix m, Matrix n) {
     matrix_multiply(m,n,n);
     return n;
 }
 
 void matrix_multiply_vec(const Matrix m, const Vec v, Vec r) {
-    r[0] = m[0]*v[0] + m[1]*v[1] + m[2]*v[2] + m[3]*v[3];
-    r[1] = m[4]*v[0] + m[5]*v[1] + m[6]*v[2] + m[7]*v[3];
-    r[2] = m[8]*v[0] + m[9]*v[1] + m[10]*v[2] + m[11]*v[3];
-    r[3] = m[12]*v[0] + m[13]*v[1] + m[14]*v[2] + m[15]*v[3];
+    Vec t;
+    t[0] = m[0]*v[0] + m[1]*v[1] + m[2]*v[2] + m[3]*v[3];
+    t[1] = m[4]*v[0] + m[5]*v[1] + m[6]*v[2] + m[7]*v[3];
+    t[2] = m[8]*v[0] + m[9]*v[1] + m[10]*v[2] + m[11]*v[3];
+    t[3] = m[12]*v[0] + m[13]*v[1] + m[14]*v[2] + m[15]*v[3];
+
+    r[0] = t[0]; r[1] = t[1]; r[2] = t[2]; r[3] = t[3];
 }
 
-float* mmul_vec(const Matrix m, Vec v) {
+MatrixP mmul_vec(const Matrix m, Vec v) {
     matrix_multiply_vec(m,v,v);
     return v;
 }
@@ -399,4 +437,21 @@ void matrix_scaling(const matrix_initfunc f, const Vec v, Matrix r) {
     r[0] = v[0];
     r[5] = v[1];
     r[10] = v[2];
+}
+
+void matrix_transpose(const Matrix m, Matrix r) {
+    Matrix t;
+    t[0] = m[0];  t[4] = m[1];  t[8]  = m[2];  t[12] = m[3];
+    t[1] = m[4];  t[5] = m[5];  t[9]  = m[6];  t[13] = m[7];
+    t[2] = m[8];  t[6] = m[9];  t[10] = m[10]; t[14] = m[11];
+    t[3] = m[12]; t[7] = m[13]; t[11] = m[14]; t[15] = m[15];
+
+    for(int i = 0; i < 16; i++) {
+        r[i] = t[i];
+    }
+}
+
+MatrixP mtransp(Matrix m) {
+    matrix_transpose(m, m);
+    return m;
 }
--- matrix.h
+++ matrix.h
@@ -27,32 +27,38 @@
 typedef void (*matrix_initfunc)(Matrix m);
 
 // vector
+void vector_copy(const Vec v, Vec r);
+
 void vector_add(const Vec v, const Vec w, Vec r);
+VecP vadd(const Vec v, Vec w);
+
 void vector_add3f(const Vec v, const Vec3f w, Vec r);
 void vector3f_add3f(const Vec3f v, const Vec3f w, Vec3f r);
 
 void vector_subtract(const Vec v, const Vec w, Vec r);
 
 void vector_multiply(const Vec v, const Vec w, Matrix m);
-void vector_multiply1f(const Vec v, float w, Vec r);
-float* vmul1f(Vec v, float w);
+VecP vmul(Vec v, Matrix w);
+
+void vector_multiply1f(float v, const Vec w, Vec r);
+VecP vmul1f(float v, Vec w);
 
 void vector_invert(const Vec v, Vec r);
-float* vinv(Vec v);
+VecP vinv(Vec v);
 
 void vector_dot(const Vec v, const Vec w, float* r);
 float vdot(const Vec v, const Vec w);
 
 void vector_cross(const Vec v, const Vec w, Vec r);
-float* vcross(const Vec v, Vec w);
+VecP vcross(const Vec v, Vec w);
 
-void vector_length(const Vec v, float* r);
-float vlength(const Vec v);
+void vector_magnitude(const Vec v, VecP r);
+float vmagnitude(const Vec v);
 
 void vector_normalize(const Vec v, Vec r);
-float* vnormalize(Vec v);
+VecP vnormalize(Vec v);
 
-void vector_angle(const Vec v, const Vec w, float* r);
+void vector_angle(const Vec v, const Vec w, VecP r);
 float vangle(const Vec v, const Vec w);
 
 void vector_isnull(const Vec v, bool* r);
@@ -61,26 +67,36 @@ bool vnullp(const Vec v);
 void vector_perpendicular(const Vec v, Vec r);
 
 // matrix creation
+void matrix_copy(const Matrix m, Matrix r);
+
 void matrix_perspective(float left, float right, float top, float bottom, float zNear, float zFar, Matrix m);
 void matrix_orthographic(float left, float right, float top, float bottom, float zNear, float zFar, Matrix m);
 
 void matrix_identity(Matrix m);
+//MatrixP midentity(Matrix m);
 
 void matrix_invert(const Matrix m, Matrix r, double* det);
-float* minv(Matrix m, double* det);
+MatrixP minv(Matrix m, double* det);
 
 // matrix op
 void matrix_multiply(const Matrix m, const Matrix n, Matrix r);
-float* mmul(const Matrix m, Matrix n);
+MatrixP mmul(const Matrix m, Matrix n);
 
 void matrix_multiply_vec(const Matrix m, const Vec v, Vec r);
-float* mmul_vec(const Matrix m, Vec v);
+MatrixP mmul_vec(const Matrix m, Vec v);
 
 void matrix_translate(const Matrix m, const Vec v, Matrix r);
+//MatrixP mtranslate(const Vec v, Matrix m);
 
 void matrix_rotate(const Matrix m, const Quat q, Matrix r);
+//MatrixP mrotate(const Quat q, Matrix m);
 
 void matrix_scale(const Matrix m, const Vec v, Matrix r);
+//MatrixP mscale(const Vec v, Matrix m);
+
 void matrix_scaling(const matrix_initfunc f, const Vec v, Matrix r);
+
+void matrix_transpose(const Matrix m, Matrix r);
+MatrixP mtransp(Matrix m);
 
 #endif
