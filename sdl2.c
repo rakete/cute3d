19a6f3d00403a6eadf9d6ee8aac3b80f300b493c
Andreas Raster <lazor@affenbande.org>
2015-02-15 03:43:26 +0100

    fix multisampling, use init_ogl in sdl2.c
    - wrap sdl2.c calls in debug macros

--- sdl2.c
+++ sdl2.c
@@ -14,45 +14,42 @@ int init_sdl2() {
 }
 
 void sdl2_window(const char* title, int x, int y, int width, int height, SDL_Window** window) {
-    *window = SDL_CreateWindow(title, x, y, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    sdl2_debug({
+            // these must be set before creating a window
+            SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+            SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+
+            *window = SDL_CreateWindow(title, x, y, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+        });
 }
 
 void sdl2_glcontext(SDL_Window* window, SDL_GLContext** context) {
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-
-    *context = SDL_GL_CreateContext(window);
-    const char* gl_version = (const char*)glGetString(GL_VERSION);
-    printf("%s\n", gl_version);
-
-    glEnable(GL_MULTISAMPLE);
-
     int width,height;
-    SDL_GL_GetDrawableSize(window, &width, &height);
-    glViewport(0,0,width,height);
-    glDepthMask(GL_TRUE);
-    glDepthFunc(GL_LESS);
-    glEnable(GL_DEPTH_TEST);
+    sdl2_debug({
+            *context = SDL_GL_CreateContext(window);
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+            SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+
+            SDL_GL_GetDrawableSize(window, &width, &height);
+        });
+
+    init_ogl(width, height);
 
     init_shader();
 }
 
 void sdl2_orbit_create(SDL_Window* window, Vec origin, Vec translation, struct Camera* camera) {
     int width,height;
-    SDL_GL_GetDrawableSize(window, &width, &height);
+    sdl2_debug( SDL_GL_GetDrawableSize(window, &width, &height) );
 
     camera_create(perspective, width, height, camera);
     //camera_projection(camera, orthographic_zoom);
@@ -73,14 +70,17 @@ double sdl2_time() {
 double sdl2_time_delta() {
     static Uint64 then = 0;
 
-    Uint64 now = SDL_GetPerformanceCounter();
+    Uint64 now;
+    sdl2_debug( now = SDL_GetPerformanceCounter() );
 
     if( then == 0 ) {
         then = now;
         return 0.0;
     }
 
-    Uint64 frequency = SDL_GetPerformanceFrequency();
+    Uint64 frequency;
+    sdl2_debug( frequency = SDL_GetPerformanceFrequency() );
+
     double delta = (double)(now - then) / (double)frequency;
     then = now;
 
