166c466e3b5f4271e1f6216b22b4b3bc0fce264a
Andreas Raster <lazor@affenbande.org>
2015-03-16 23:55:40 +0100

    make ogl_debug macro call glGetError repeatedly until there are no more errors

--- ogl.h
+++ ogl.h
@@ -14,33 +14,34 @@
 #define ogl_debug(line) do {                                            \
         line;                                                           \
         GLenum error = glGetError();                                    \
-        if( error != GL_NO_ERROR ) {                                    \
+        while( error != GL_NO_ERROR ) {                                 \
             printf("%s@%d:::::\n%s\n", __FILE__, __LINE__, ogl_stringify(line)); \
-        }                                                               \
-        switch(error) {                                                 \
-            case GL_NO_ERROR: break;                                    \
-            case GL_INVALID_ENUM:                                       \
-                printf("glGetError: GL_INVALID_ENUM\n");                \
-                break;                                                  \
-            case GL_INVALID_VALUE:                                      \
-                printf("glGetError: GL_INVALID_VALUE\n");               \
-                break;                                                  \
-            case GL_INVALID_OPERATION:                                  \
-                printf("glGetError: GL_INVALID_OPERATION\n");           \
-                break;                                                  \
-            case GL_INVALID_FRAMEBUFFER_OPERATION:                      \
-                printf("glGetError: GL_INVALID_FRAMEBUFFER_OPERATION\n"); \
-                break;                                                  \
-            case GL_OUT_OF_MEMORY:                                      \
-                printf("glGetError: GL_OUT_OF_MEMORY\n");               \
-                break;                                                  \
-            case GL_STACK_UNDERFLOW:                                    \
-                printf("glGetError: GL_STACK_UNDERFLOW\n");             \
-                break;                                                  \
-            case GL_STACK_OVERFLOW:                                     \
-                printf("glGetError: GL_STACK_OVERFLOW\n");              \
-                break;                                                  \
-            default: break;                                             \
+            switch(error) {                                             \
+                case GL_NO_ERROR: break;                                \
+                case GL_INVALID_ENUM:                                   \
+                    printf("glGetError: GL_INVALID_ENUM\n");            \
+                    break;                                              \
+                case GL_INVALID_VALUE:                                  \
+                    printf("glGetError: GL_INVALID_VALUE\n");           \
+                    break;                                              \
+                case GL_INVALID_OPERATION:                              \
+                    printf("glGetError: GL_INVALID_OPERATION\n");       \
+                    break;                                              \
+                case GL_INVALID_FRAMEBUFFER_OPERATION:                  \
+                    printf("glGetError: GL_INVALID_FRAMEBUFFER_OPERATION\n"); \
+                    break;                                              \
+                case GL_OUT_OF_MEMORY:                                  \
+                    printf("glGetError: GL_OUT_OF_MEMORY\n");           \
+                    break;                                              \
+                case GL_STACK_UNDERFLOW:                                \
+                    printf("glGetError: GL_STACK_UNDERFLOW\n");         \
+                    break;                                              \
+                case GL_STACK_OVERFLOW:                                 \
+                    printf("glGetError: GL_STACK_OVERFLOW\n");          \
+                    break;                                              \
+                default: break;                                         \
+            }                                                           \
+            error = glGetError();                                       \
         }                                                               \
         assert(error == GL_NO_ERROR);                                   \
     } while(0)
