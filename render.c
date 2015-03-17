bcd09bd5d84afb3bace340410be33c4831519022
Andreas Raster <lazor@affenbande.org>
2015-03-17 22:15:39 +0100

    add ogl_debug to render code, still no complaints

--- render.c
+++ render.c
@@ -34,42 +34,42 @@ void render_mesh(const struct Mesh* mesh, const struct Shader* shader, const str
     if( shader->location[SHADER_MVP_MATRIX].id > -1) {
         mvp_loc = shader->location[SHADER_MVP_MATRIX].id;
     } else {
-        mvp_loc = glGetUniformLocation(shader->program, "mvp_matrix");
+        ogl_debug( mvp_loc = glGetUniformLocation(shader->program, "mvp_matrix") );
     }
 
     if( mvp_loc > -1 ) {
         Mat mvp_matrix;
         mat_mul(model_matrix, view_matrix, mvp_matrix);
         mat_mul(mvp_matrix, projection_matrix, mvp_matrix);
-        glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, mvp_matrix);
+        ogl_debug( glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, mvp_matrix) );
     } else {
         GLint projection_loc = -1;
         if( shader->location[SHADER_PROJECTION_MATRIX].id > -1 ) {
             projection_loc = shader->location[SHADER_PROJECTION_MATRIX].id;
         } else {
-            projection_loc = glGetUniformLocation(shader->program, "projection_matrix");
+            ogl_debug( projection_loc = glGetUniformLocation(shader->program, "projection_matrix") );
         }
 
         if( projection_loc > -1 ) {
-            glUniformMatrix4fv(projection_loc, 1, GL_FALSE, projection_matrix);
+            ogl_debug( glUniformMatrix4fv(projection_loc, 1, GL_FALSE, projection_matrix) );
         }
 
         GLint view_loc = -1;
         if( shader->location[SHADER_VIEW_MATRIX].id > -1 ) {
             view_loc = shader->location[SHADER_VIEW_MATRIX].id;
         } else {
-            view_loc = glGetUniformLocation(shader->program, "view_matrix");
+            ogl_debug( view_loc = glGetUniformLocation(shader->program, "view_matrix") );
         }
 
         if( view_loc > -1 ) {
-            glUniformMatrix4fv(view_loc, 1, GL_FALSE, view_matrix);
+            ogl_debug( glUniformMatrix4fv(view_loc, 1, GL_FALSE, view_matrix) );
         }
 
         GLint model_loc = -1;
         if( shader->location[SHADER_MODEL_MATRIX].id > -1 ) {
             model_loc = shader->location[SHADER_MODEL_MATRIX].id;
         } else {
-            model_loc = glGetUniformLocation(shader->program, "model_matrix");
+            ogl_debug( model_loc = glGetUniformLocation(shader->program, "model_matrix") );
         }
 
         if( ! model_matrix ) {
@@ -79,7 +79,7 @@ void render_mesh(const struct Mesh* mesh, const struct Shader* shader, const str
         }
 
         if( model_loc > -1 ) {
-            glUniformMatrix4fv(model_loc, 1, GL_FALSE, model_matrix);
+            ogl_debug( glUniformMatrix4fv(model_loc, 1, GL_FALSE, model_matrix) );
         }
     }
 
@@ -87,14 +87,14 @@ void render_mesh(const struct Mesh* mesh, const struct Shader* shader, const str
     if( shader->location[SHADER_NORMAL_MATRIX].id > -1 ) {
         normal_loc = shader->location[SHADER_NORMAL_MATRIX].id;
     } else {
-        normal_loc = glGetUniformLocation(shader->program, "normal_matrix");
+        ogl_debug( normal_loc = glGetUniformLocation(shader->program, "normal_matrix") );
     }
 
     if( (! free_model_matrix) && normal_loc > -1 ) {
         Mat normal_matrix;
         mat_invert(model_matrix, NULL, normal_matrix);
         mat_transpose(normal_matrix, normal_matrix);
-        glUniformMatrix4fv(normal_loc, 1, GL_FALSE, normal_matrix);
+        ogl_debug( glUniformMatrix4fv(normal_loc, 1, GL_FALSE, normal_matrix) );
     }
 
     if( free_model_matrix ) {
@@ -103,36 +103,31 @@ void render_mesh(const struct Mesh* mesh, const struct Shader* shader, const str
 
     GLint loc[NUM_BUFFERS];
     for( int array_id = 0; array_id < NUM_BUFFERS; array_id++ ) {
-        loc[array_id] = glGetAttribLocation(shader->program, shader->attribute[array_id].name);
-        /* if( strlen(shader->attribute[array_id].name) > 0 ) { */
-        /*     printf("%s: %d\n", shader->attribute[array_id].name, loc); */
-        /* } */
+        ogl_debug( loc[array_id] = glGetAttribLocation(shader->program, shader->attribute[array_id].name) );
+
         if( mesh->vbo->buffer[array_id].id && loc[array_id] > -1 ) {
-            glEnableVertexAttribArray(loc[array_id]);
-
-            glBindBuffer(GL_ARRAY_BUFFER, mesh->vbo->buffer[array_id].id);
-
             GLint c_num = mesh->vbo->components[array_id].size;
             GLint c_type = mesh->vbo->components[array_id].type;
             GLsizei c_bytes = mesh->vbo->components[array_id].bytes;
             GLsizei offset = mesh->offset * c_num * c_bytes;
-            //printf("%lu %lu %lu %lu\n", c_num, mesh->offset, c_bytes, offset);
 
-            glVertexAttribPointer(loc[array_id], c_num, c_type, GL_FALSE, 0, (void*)(intptr_t)offset);
+            ogl_debug( glEnableVertexAttribArray(loc[array_id]);
+                       glBindBuffer(GL_ARRAY_BUFFER, mesh->vbo->buffer[array_id].id);
+                       glVertexAttribPointer(loc[array_id], c_num, c_type, GL_FALSE, 0, (void*)(intptr_t)offset); );
         }
     }
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->primitives.buffer->id);
-    glDrawElements(mesh->primitives.type, mesh->primitives.buffer->used, mesh->index.type, 0);
+    ogl_debug( glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->primitives.buffer->id);
+               glDrawElements(mesh->primitives.type, mesh->primitives.buffer->used, mesh->index.type, 0); );
 
     for( int array_id = 0; array_id < NUM_BUFFERS; array_id++ ) {
         if( loc[array_id] > -1 ) {
-            glDisableVertexAttribArray(loc[array_id]);
+            ogl_debug( glDisableVertexAttribArray(loc[array_id]) );
         }
     }
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    ogl_debug( glBindBuffer(GL_ARRAY_BUFFER, 0);
+               glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); );
 }
 
 void render_shader_flat(struct Shader* shader) {
