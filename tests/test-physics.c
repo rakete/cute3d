3ca8686927e1fbeb20c3d5d4d0a7a5165621f279
Andreas Raster <lazor@affenbande.org>
2015-05-26 03:23:26 +0200

    test-physics with all the latest bells and whistles

--- tests/test-physics.c
+++ tests/test-physics.c
@@ -67,10 +67,12 @@ void physics_forces(struct Physics state, float t, float dt, Vec force, Vec torq
 
     force[1] -= 9.81f;
 
-    //torque[2] += 1.0;
+    torque[0] += 0.0;
+    torque[1] += 0.0;
+    torque[2] += 0.8;
 
-    const float linear = 0.01f;
-    const float angular = 0.01f;
+    const float linear = 0.1f;
+    const float angular = 0.1f;
 
     //force -= linear * state.velocity;
     vec_sub(force, vmul1f(state.linear_velocity, linear), force);
@@ -105,7 +107,8 @@ int main(int argc, char *argv[]) {
     }
 
     SDL_Window* window;
-    sdl2_window("test-physics", 0, 0, 800, 600, &window);
+    sdl2_window("test-physics", 0, 0, 1920, 1080, &window);
+    SDL_SetWindowFullscreen(window, SDL_TRUE);
 
     SDL_GLContext* context;
     sdl2_glcontext(window, &context);
@@ -116,13 +119,14 @@ int main(int argc, char *argv[]) {
 
     struct BouncingSphere entity;
 
-    /* Cube */
-    solid_sphere16(1.0, &entity.solid);
-    mesh_from_solid((struct Solid*)&entity.solid, (Color){1.0, 0.0, 0.0, 1.0}, &entity.mesh);
+    /* Sphere */
+    float radius = 2.0f;
+    solid_sphere16(radius, &entity.solid);
+    mesh_from_solid((struct Solid*)&entity.solid, (Color){0.7, 0.1, 0.0, 1.0}, &entity.mesh);
 
     Mat inertia;
     float mass = 1;
-    physics_sphere_inertia(1.0, mass, inertia);
+    physics_sphere_inertia(radius, mass, inertia);
     physics_create(mass, inertia, &entity.current);
 
     vec_copy((Vec){0.0f, 10.0f, 0.0f, 1.0f}, entity.current.pivot.position);
@@ -130,12 +134,13 @@ int main(int argc, char *argv[]) {
     vec_copy((Vec){0.0f, 0.0f, 0.0f, 1.0f}, entity.current.angular_momentum);
 
     entity.previous = entity.current;
-    collider_sphere(&entity.current.pivot, 1.0, &entity.collider);
+    collider_sphere(radius, &entity.current.pivot, &entity.collider);
 
     /* Ground */
     struct Ground ground;
     pivot_create(&ground.pivot);
-    collider_plane(&ground.pivot, (Vec){0.0, 1.0, 0.0, 1.0}, 0.0, &ground.collider);
+    collider_plane(((Vec){0.0, 1.0, 0.0, 1.0}), -4.0, &ground.pivot, &ground.collider);
+    //vec_copy((Vec){0.0, -4.0, 0.0, 1.0}, ground.pivot.position);
 
     /* Shader */
     if( ! init_shader() ) {
@@ -145,15 +150,15 @@ int main(int argc, char *argv[]) {
     struct Shader shader;
     render_shader_flat(&shader);
 
-    Vec light_direction = { 0.0, 0.2, -0.8 };
+    Vec light_direction = { 0.0, -1.0, 0.0, 1.0 };
     shader_uniform(&shader, SHADER_LIGHT_DIRECTION, "light_direction", "3f", light_direction);
 
-    Color ambiance = { 0.1, 0.0, 0.05, 1.0 };
+    Color ambiance = { 0.0, 0.0, 0.05, 1.0 };
     shader_uniform(&shader, SHADER_AMBIENT_COLOR, "ambiance", "4f", ambiance);
 
     /* Matrices */
     struct Camera camera;
-    sdl2_orbit_create(window, (Vec){12.0,8.0,32.0,1.0}, (Vec){0.0,0.0,0.0,1.0}, 1.0f, 1000.0f, &camera);
+    sdl2_orbit_create(window, (Vec){0.0, 12.0, 32.0, 1.0}, (Vec){0.0,0.0,0.0,1.0}, 1.0f, 1000.0f, &camera);
 
     Mat projection_mat, view_mat;
     camera_matrices(&camera, projection_mat, view_mat);
@@ -162,13 +167,25 @@ int main(int argc, char *argv[]) {
 
     /* Time */
     struct GameTime time;
-    gametime_create(1.0f / 30.0f, &time);
+    gametime_create(1.0f / 60.0f, &time);
 
     sdl2_debug( SDL_GL_SetSwapInterval(0) );
 
     /* Text */
     struct Character symbols[256];
     ascii_create(symbols);
+
+    /* collision detection state */
+    size_t world_size = 2;
+
+    struct Collider* world_colliders[world_size];
+    struct Physics* world_bodies[world_size];
+    size_t candidates[world_size];
+    size_t candidates_size = 0;
+
+    struct Collision collisions[world_size];
+    struct Physics* bodies[world_size];
+    size_t collisions_size = 0;
 
     /* Eventloop */
     while (true) {
@@ -190,49 +207,48 @@ int main(int argc, char *argv[]) {
         gametime_advance(sdl2_time_delta(), &time);
 
         ogl_debug( glClearDepth(1.0f);
-                   glClearColor(.0f, .0f, .0f, 1.0f);
+                   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); );
 
         show_fps_counter(time.frame);
         show_time(time.t);
 
-        Mat grid_transform;
-        mat_identity(grid_transform);
+        Mat grid_transform = IDENTITY_MAT;
 
-        Quat grid_rotation;
-        quat_rotating((Vec){1.0, 0.0, 0.0, 1.0}, PI/2, grid_rotation);
+        Quat grid_rotation1;
+        quat_rotating_vec((Vec){0.0, 0.0, 1.0, 1.0}, (Vec){0.0, 1.0, 0.0, 1.0}, grid_rotation1);
+        Quat grid_rotation2;
+        quat_rotating_vec((Vec){0.0, 1.0, 0.0, 1.0}, ground.collider.normal, grid_rotation2);
+        mat_rotate(grid_transform, grid_rotation1, grid_transform);
+        mat_rotate(grid_transform, grid_rotation2, grid_transform);
 
-        mat_rotate(grid_transform, grid_rotation, grid_transform);
+        Vec point_on_grid = { ground.collider.normal[0] * ground.collider.offset,
+                              ground.collider.normal[1] * ground.collider.offset,
+                              ground.collider.normal[2] * ground.collider.offset,
+                              1.0 };
+        mat_translate(grid_transform, point_on_grid, grid_transform);
 
         draw_grid(480.0f, 480.0f, 48, (Color){0.5, 0.5, 0.5, 1.0}, projection_mat, view_mat, grid_transform);
         draw_grid(20.0f, 20.0f, 10, (Color){0.5, 0.5, 0.5, 1.0}, projection_mat, view_mat, grid_transform);
         draw_grid(12.0f, 12.0f, 12, (Color){0.5, 0.5, 0.5, 1.0}, projection_mat, view_mat, grid_transform);
 
-        /* draw_grid4(1024.0f, 1024.0f, 1024, (Color){0.5, 0.5, 0.5, 1.0}, projection_mat, view_mat, grid_transform); */
-
         entity.previous = entity.current;
         while( gametime_integrate(&time) ) {
-
-            size_t world_size = 2;
-
-            struct Collider* world_colliders[world_size];
+            /* resolve collisions */
             world_colliders[0] = (struct Collider*)&entity.collider;
             world_colliders[1] = (struct Collider*)&ground.collider;
 
-            struct Physics* world_bodies[world_size];
             world_bodies[0] = (struct Physics*)&entity.current;
             world_bodies[1] = NULL;
 
-            size_t candidates[world_size];
-            size_t candidates_size = collisions_broad(0, world_size, world_colliders, candidates);
+            candidates_size = collisions_broad(0, world_size, world_colliders, candidates);
 
-            struct Collision collisions[candidates_size];
-            struct Physics* bodies[candidates_size];
             collisions_prepare(candidates_size, collisions);
 
-            size_t collisions_size = collisions_narrow(0, world_size, world_colliders, world_bodies, candidates_size, candidates, bodies, collisions);
+            collisions_size = collisions_narrow(0, world_size, world_colliders, world_bodies, candidates_size, candidates, bodies, collisions);
             entity.current = collisions_resolve(entity.previous, entity.current, collisions_size, bodies, collisions, time.scale * time.dt);
 
+            /* physics integration */
             entity.previous = entity.current;
             entity.current = physics_integrate(entity.current, time.t, time.scale * time.dt, &physics_forces);
         }
@@ -245,25 +261,50 @@ int main(int argc, char *argv[]) {
         draw_normals_array(entity.solid.vertices,
                            entity.solid.normals,
                            entity.solid.solid.size,
-                           0.5,
-                           (Color){ 1.0,0.0,1.0,1.0 },
+                           0.25,
+                           (Color){1.0, 0.0, 1.0, 1.0},
                            projection_mat,
                            view_mat,
                            entity.current.world_transform);
 
         glDisable(GL_DEPTH_TEST);
-        draw_pivot(4.0,
+
+        Mat translation_mat;
+        mat_translating(entity.current.pivot.position, translation_mat);
+
+        draw_pivot(1.0,
                    projection_mat,
                    view_mat,
-                   entity.current.world_transform);
-        draw_physics(entity.current.pivot.position,
-                     entity.current.mass,
-                     entity.current.inertia,
-                     entity.current.linear_velocity,
-                     entity.current.angular_velocity,
-                     1.0,
-                     projection_mat,
-                     view_mat);
+                   translation_mat);
+
+        /* draw physics */
+        draw_vec(entity.current.linear_velocity,
+                 (Vec){0.0, 0.0, 0.0, 1.0},
+                 1.0f,
+                 1.0f,
+                 (Color){1.0, 1.0, 0.0, 1.0},
+                 projection_mat,
+                 view_mat,
+                 translation_mat);
+        draw_quat(entity.current.spin,
+                  1.0f,
+                  (Color){1.0, 1.0, 1.0, 1.0},
+                  (Color){0.0, 0.2, 0.4, 1.0},
+                  projection_mat,
+                  view_mat,
+                  translation_mat);
+
+        /* draw contacts */
+        for( unsigned int i = 0; i < collisions_size; i++ ) {
+            for( unsigned int j = 0; j < collisions[i].num_contacts; j++ ) {
+                Vec* contact_normal = &collisions[i].contact[j].normal;
+                Vec* contact_point = &collisions[i].contact[j].point;
+                float penetration = collisions[i].contact[j].penetration;
+
+                draw_contact(*contact_point, *contact_normal, penetration, 1.0, projection_mat, view_mat, translation_mat);
+            }
+        }
+
         glEnable(GL_DEPTH_TEST);
 
         show_render(NULL, 10, camera);
@@ -272,5 +313,6 @@ int main(int argc, char *argv[]) {
     }
 
 done:
+    SDL_Quit();
     return 0;
 }
