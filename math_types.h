643d226c964891f6d8b4acb1b0a17be28cc3e4c9
Andreas Raster <lazor@affenbande.org>
2016-02-09 22:23:50 +0100

    going to remove unused math_types

--- math_types.h
+++ math_types.h
@@ -33,44 +33,19 @@ typedef float Vec[4];
 typedef float Vec2f[2];
 typedef float Vec3f[3];
 typedef float Vec4f[4];
-typedef int32_t Vec3i[3];
-typedef int32_t Vec4i[4];
 
 typedef float Mat[16];
-typedef float Mat4f[16];
-typedef float Mat4i[16];
 
 typedef float Quat[4];
 
 typedef uint8_t Color[4];
-typedef float Color4f[4];
-typedef uint8_t Color4ub[4];
 
 typedef float Texcoord[2];
 
-#define vec(v) vec4f(v)
-#define vecf(v) vec4f(v)
-#define vec2f(v) (Vec3f){(float)v[0], (float)v[1]}
-#define vec3f(v) (Vec3f){(float)v[0], (float)v[1], (float)v[2]}
-#define vec4f(v) (Vec4f){(float)v[0], (float)v[1], (float)v[2], (float)v[3]}
-
-#define x_(v) v[0]
-#define y_(v) v[1]
-#define z_(v) v[2]
-#define w_(v) v[3]
-
-#define matf(m) mat4x4f(m)
-#define mat4x4f(m)                                                      \
-    (Mat){(float)m[0], (float)m[1], (float)m[2], (float)m[3],           \
-            (float)m[4], (float)m[5], (float)m[6], (float)m[7],         \
-            (float)m[8], (float)m[9], (float)m[10], (float)m[11],       \
-            (float)m[12], (float)m[13], (float)m[14], (float)m[15] }
-
-
 #define NULL_VEC { 0, 0, 0, 1 }
-#define NULL_VEC3F { 0, 0, 0 }
-#define NULL_MAT { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+#define NULL_MAT { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }
 
+#define IDENTITY_VEC { 1, 1, 1, 1 }
 #define IDENTITY_MAT { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 }
 
 #define X_AXIS { 1, 0, 0, 1 }
