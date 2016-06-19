7a476789f3c8bbe7b7748c1df34357c5a9ba37d2
Andreas Raster <lazor@affenbande.org>
2016-06-19 03:47:51 +0200

    in progress translating Makefile to python

--- ninja_build.py
+++ ninja_build.py
@@ -1,13 +1,65 @@
 import ninja_syntax
 import glob
+import platform
+import subprocess
+import sys
 
 f = open("build.ninja","w+")
 w = ninja_syntax.Writer(f, 256)
 
-w.variable("warnings", "-Wall")
+build_platform = platform.system().lower()
+build_toolset = "gcc"
+build_architecture = platform.machine().lower()
+
+if len(sys.argv) > 1:
+    user_toolset = sys.argv[1].lower()
+    if user_toolset == "gcc" or user_toolset == "clang":
+        build_toolset = user_toolset
+    if (user_toolset == "msvc" or user_toolset == "mingw") and build_platform == "windows":
+        build_toolset = user_toolset
+
+print "build_platform: " + build_platform
+print "build_toolset: " + build_toolset
+print "build_architecture: " + build_architecture
+
+sdl2_cflags = ""
+sdl2_libs = ""
+if build_platform == "linux":
+    sdl2_cflags = subprocess.check_output(["bash", "sdl2-config", "--cflags"]).rstrip()
+    sdl2_libs = subprocess.check_output(["bash", "sdl2-config", "--libs"]).rstrip()
+elif build_toolset == "mingw":
+    sdl2_cflags = ""
+    sdl2_libs = ""
+elif build_toolset == "msvc":
+    sdl2_cflags = ""
+    sdl2_libs = ""
+
+print "sdl2_cflags: " + sdl2_cflags
+print "sdl2_libs: " + sdl2_libs
+w.variable("sdl2_cflags", sdl2_cflags)
+w.variable("sdl2_libs", sdl2_libs)
 w.newline()
 
-w.rule("gcc", "gcc $warnings -c $in -o $out")
+features = "-pg -DDEBUG"
+optimization = "-fPIC -flto=4 -march=native"
+warnings = "-Wall -Wmaybe-uninitialized -Wsign-conversion -Wno-missing-field-initializers -Wno-missing-braces -pedantic -Wvla"
+print "features: " + features
+print "optimization: " + optimization
+print "warnings: " + warnings
+w.variable("features", features)
+w.variable("optimization", optimization)
+w.variable("warnings", warnings)
+w.newline()
+
+cflags = "-std=c99 $warning $features $sdl2_cflags $optimization"
+ldflags = "-lm -lGL $sdl2_libs"
+print "cflags: " + cflags
+print "ldflags: " + ldflags
+w.variable("cflags", cflags)
+w.variable("ldflags", ldflags)
+w.newline()
+
+w.rule("cc", "gcc $cflags $ldflags -c $in -o $out")
 w.newline()
 
 sources = glob.glob("*.c")
@@ -15,5 +67,5 @@ sources.remove("driver_allegro.c")
 
 for input in sources:
     output = input.replace(".c", ".o")
-    w.build(output, "gcc", input)
+    w.build(output, "cc", input)
 w.newline()
