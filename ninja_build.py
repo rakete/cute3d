850b627e3165c9c95c3eb3114b54350430707daa
Andreas Raster <lazor@affenbande.org>
2016-06-21 01:42:29 +0200

    add basefilename to compile rules, add shader copying/validation
    - not yet working correcly
    - also rename cc -> compile, ld -> link etc. better to distinguish from
      other crap

--- ninja_build.py
+++ ninja_build.py
@@ -100,15 +100,81 @@ print "ldflags: " + ldflags
 f = open("build.ninja", "w+")
 w = ninja_syntax.Writer(f, 127)
 
+w.variable("basefilename", "\"$$in\"")
+w.newline()
+
+def command_exists(cmd):
+    return any(
+        os.access(os.path.join(path, cmd), os.X_OK) or
+        os.access(os.path.join(path, cmd + ".exe"), os.X_OK)
+        for path in os.environ["PATH"].split(os.pathsep)
+    )
+
+if build_platform == "windows":
+    w.rule(name="copy", command="cmd /c copy.exe $in $out >nul")
+else:
+    w.rule(name="copy", command="cp $in $out")
+w.newline()
+
+if build_platform == "windows":
+    os.chdir(source_directory)
+    dlls = glob.glob("*.dll")
+    os.chdir(current_directory)
+
+    for d in dlls:
+        w.build(d, "copy", os.path.join(source_directory, d))
+    w.newline()
+
+shader_directory = os.path.relpath(os.path.join(source_directory, "shader") , current_directory)
+
+if build_platform == "windows":
+    w.rule(name="make_shader_directory", command="cmd /c mkdir shader & copy NUL $out")
+else:
+    w.rule(name="make_shader_directory", command="mkdir -p shader; touch $out")
+w.newline()
+
+w.build("shader\directory", "make_shader_directory")
+w.newline()
+
+if command_exists("glsl-validate.py"):
+    if build_platform == "windows":
+        w.rule(name="copy_shader", command="cmd /c glsl-validate.py $in & for %I in ($in) do copy %I shader >nul")
+    else:
+        w.rule(name="copy_shader", command="bash -c \"glsl-validate.py $in; cp $in shader\"")
+else:
+    if build_platform == "windows":
+        w.rule(name="copy_shader", command="cmd /c for %I in ($in) do copy %I shader >nul")
+    else:
+        w.rule(name="copy_shader", command="bash -c \"cp $in shader\"")
+w.newline()
+
+def pairwise(it):
+    it = iter(it)
+    while True:
+        yield next(it), next(it)
+
+os.chdir(shader_directory)
+shader_filenames = sorted(glob.glob("*.[frag|vert]*"))
+os.chdir(current_directory)
+shaders = []
+for frag_shader_filename, vert_shader_filename in pairwise(shader_filenames):
+    vert_shader = os.path.join("shader", vert_shader_filename)
+    frag_shader = os.path.join("shader", frag_shader_filename)
+    w.build(vert_shader, "copy_shader", os.path.join(shader_directory, vert_shader_filename), "shader\directory")
+    w.build(frag_shader, "copy_shader", os.path.join(shader_directory, frag_shader_filename), "shader\directory")
+    shaders.append(vert_shader)
+    shaders.append(frag_shader)
+w.newline()
+
 if build_toolset == "mingw" or build_toolset == "gcc":
     # -c and /c in gcc and cl.exe mean: compile without linking
-    w.rule(name="cc", command="gcc -MMD -MF $out.d -c $in -o $out " + cflags, deps="gcc", depfile="$out.d")
+    w.rule(name="compile", command="gcc -MMD -MF $out.d -c $in -o $out -DBASE_FILE_NAME=\"$basefilename\" " + cflags, deps="gcc", depfile="$out.d")
     w.newline()
-    w.rule(name="ld", command="gcc $in -o $out " + ldflags)
+    w.rule(name="link", command="gcc $in -o $out " + ldflags)
     w.newline()
 elif build_toolset == "msvc":
     # - needs /FS to enable synchronous writes to pdb database
-    w.rule(name="cc", command="cl.exe /showIncludes /FS /c $in /Fo$out " + cflags, deps="msvc")
+    w.rule(name="compile", command="cl.exe /showIncludes /FS /c $in /Fo$out " + cflags, deps="msvc")
     w.newline()
 
 os.chdir(source_directory)
@@ -119,7 +185,8 @@ os.chdir(current_directory)
 objects = []
 for c in sources:
     o = c.replace(".c", ".o")
-    w.build(o, "cc", os.path.join(source_directory, c))
+    w.build(o, "compile", os.path.join(source_directory, c))
+    w.variable("  basefilename", c)
     objects.append(o)
 w.newline()
 
@@ -133,7 +200,13 @@ test_sources = glob.glob("*.c")
 os.chdir(current_directory)
 for c in test_sources:
     o = c.replace(".c", ".o")
-    w.build(o, "cc", os.path.join(tests_directory, c))
+    w.build(o, "compile", os.path.join(tests_directory, c))
+    w.variable("  basefilename", os.path.join("tests", c))
+
     exe = c.replace(".c", "")
-    w.build(exe, "ld", [o] + objects)
+    if build_platform == "windows":
+        winexe  = exe + ".exe"
+        w.build(exe, "phony", winexe)
+        exe = winexe
+    w.build(exe, "link", [o] + objects, dlls + shaders)
     w.newline()
