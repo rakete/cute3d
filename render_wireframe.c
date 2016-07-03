bb3528bb17afa267987b9758cb4d5f995cbb4d0f
Andreas Raster <lazor@affenbande.org>
2016-07-03 23:54:26 +0200

    add render_wireframe prototype
    - eventually I am going to implement something like that

---
+++ render_wireframe.c
@@ -0,0 +1,10 @@
+#include "render_wireframe.h"
+
+void wireframe_create_from_solid(const struct Solid* solid, struct Vbo* const vbo, struct VboMesh* mesh) {
+}
+
+void wireframe_create_from_halfedgemesh(const struct HalfEdgeMesh* halfedgemesh, struct Vbo* const vbo, struct VboMesh* mesh) {
+}
+
+void wireframe_render(struct VboMesh* mesh, struct Shader* shader, const struct Camera* camera, const Mat model_matrix) {
+}
---
+++ render_wireframe.h
@@ -0,0 +1,36 @@
+/* cute3d, a simplistic opengl based engine written in C */
+/* Copyright (C) 2013 Andreas Raster */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef RENDER_WIREFRAME_H
+#define RENDER_WIREFRAME_H
+
+#include "driver_ogl.h"
+#include "driver_log.h"
+
+#include "math_camera.h"
+#include "math_pivot.h"
+
+#include "geometry_vbo.h"
+#include "geometry_solid.h"
+#include "geometry_halfedgemesh.h"
+
+void wireframe_create_from_solid(const struct Solid* solid, struct Vbo* const vbo, struct VboMesh* mesh);
+
+void wireframe_create_from_halfedgemesh(const struct HalfEdgeMesh* halfedgemesh, struct Vbo* const vbo, struct VboMesh* mesh);
+
+void wireframe_render(struct VboMesh* mesh, struct Shader* shader, const struct Camera* camera, const Mat model_matrix);
+
+#endif
