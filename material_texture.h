47185fef1730e4281ff423f24926853e0252c6fc
Andreas Raster <lazor@affenbande.org>
2016-07-07 02:33:14 +0200

    add material_texture.h with comments only so far

---
+++ material_texture.h
@@ -0,0 +1,9 @@
+#ifndef MATERIAL_TEXTURE_H
+#define MATERIAL_TEXTURE_H
+
+// - look at glTexParameter/glTexImage2D for OpenGL ES 2, there is much less stuff to worry about:
+// http://docs.gl/es2/glTexParameter and http://docs.gl/es2/glTexImage2D
+// - use structs from shadows and font that I already use, put them here and write some
+// initialization code around them
+
+#endif
