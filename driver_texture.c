6a5b5d8d50c1a1412dceaa418e149a001170821b
Andreas Raster <lazor@affenbande.org>
2016-07-18 19:29:20 +0200

    implement texture_create

--- driver_texture.c
+++ driver_texture.c
@@ -1,5 +1,22 @@
 #include "driver_texture.h"
 
+void texture_create(struct Texture* texture) {
+    texture->id = 0;
+    texture->dimension = 0;
+
+    texture->width = 0;
+    texture->height = 0;
+
+    texture->type = 0;
+    texture->format = 0;
+
+    texture->min_filter = 0;
+    texture->mag_filter = 0;
+
+    texture->wrap_s = 0;
+    texture->wrap_t = 0;
+}
+
 GLuint texture_bind(struct Texture texture, int32_t sampler_index) {
     log_assert( sampler_index >= 0 );
     log_assert( sampler_index <= MAX_SHADER_SAMPLER );
