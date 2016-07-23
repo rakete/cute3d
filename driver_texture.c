01088934ab1486e9682eae3992a44f64180f827f
Andreas Raster <lazor@affenbande.org>
2016-07-23 02:26:17 +0200

    make it so that texture_bind works with only an id

--- driver_texture.c
+++ driver_texture.c
@@ -46,14 +46,22 @@ GLuint texture_bind(struct Texture texture, int32_t sampler_index) {
     log_assert( sampler_index >= 0 );
     log_assert( sampler_index <= MAX_SHADER_SAMPLER );
     log_assert( texture.id > 0 );
-    log_assert( texture.dimension == GL_TEXTURE_2D || texture.dimension == GL_TEXTURE_CUBE_MAP );
+    log_assert( texture.dimension == 0 || texture.dimension == GL_TEXTURE_2D || texture.dimension == GL_TEXTURE_CUBE_MAP );
     log_assert( GL_TEXTURE7 == GL_TEXTURE0 + 7 );
 
     GLuint texture_unit = sampler_index % 8;
     log_assert( texture_unit < MAX_SHADER_TEXTURE_UNITS );
 
+    // - I wanted to be able to give a texture with only an id, and the rest zeroed, so that when I have some
+    // specialized code where I only have an id, I can still use this function
+    GLenum dimension = texture.dimension;
+    if( dimension == 0 ) {
+        log_warn(__FILE__, __LINE__, "assuming a GL_TEXTURE_2D in texture_bind\n");
+        dimension = GL_TEXTURE_2D;
+    }
+
     ogl_debug( glActiveTexture(GL_TEXTURE0 + texture_unit);
-               glBindTexture(texture.dimension, texture.id); );
+               glBindTexture(dimension, texture.id); );
 
     return texture_unit;
 }
