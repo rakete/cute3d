e6a9207ee912f3901d998be54fde2722b041811b
Andreas Raster <lazor@affenbande.org>
2017-01-05 22:33:36 +0100

    go through all files, add GPL header everywhere

--- geometry_polygon.c
+++ geometry_polygon.c
@@ -1,3 +1,22 @@
+/* Cute3D, a simple opengl based framework for writing interactive realtime applications */
+
+/* Copyright (C) 2013-2017 Andreas Raster */
+
+/* This file is part of Cute3D. */
+
+/* Cute3D is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* Cute3D is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with Cute3D.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include "geometry_polygon.h"
 
 void polygon_corner_area(size_t polygon_size, size_t point_size, float* polygon, size_t corner_i, float* result) {
