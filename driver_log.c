4d7dbbf05cc20dde7cacb549c09ef27c122fbad6
Andreas Raster <lazor@affenbande.org>
2017-12-14 22:48:52 +0100

    rename LOG_ANSI_COLOR to just LOG_ANSI

--- driver_log.c
+++ driver_log.c
@@ -27,12 +27,12 @@ void log_printf(const char* message, va_list args) {
 void log_message(const char* color, const char* tag, const char* filename, int32_t linenumber, const char* format, size_t n, char* message) {
     if( filename && linenumber > 0 ) {
 #ifdef DEBUG
-        snprintf(message, n, "%s:%d: %s%s" LOG_ANSI_COLOR_RESET "%s" LOG_ANSI_COLOR_RESET, filename, linenumber, color, tag, format);
+        snprintf(message, n, "%s:%d: %s%s" LOG_ANSI_RESET "%s" LOG_ANSI_RESET, filename, linenumber, color, tag, format);
 #else
-        snprintf(message, n, "%s%s" LOG_ANSI_COLOR_RESET "%s", color, tag, format);
+        snprintf(message, n, "%s%s" LOG_ANSI_RESET "%s", color, tag, format);
 #endif
     } else {
-        snprintf(message, n, "%s%s" LOG_ANSI_COLOR_RESET "%s", color, tag, format);
+        snprintf(message, n, "%s%s" LOG_ANSI_RESET "%s", color, tag, format);
     }
 }
 
@@ -41,7 +41,7 @@ void log_info(const char* filename, int32_t linenumber, const char* format, ...)
     va_start(args, format);
 
     char message[8192];
-    log_message(LOG_ANSI_COLOR_GREEN, "[INFO] ", filename, linenumber, format, 8192, message);
+    log_message(LOG_ANSI_GREEN, "[INFO] ", filename, linenumber, format, 8192, message);
 
     log_printf(message, args);
     va_end(args);
@@ -52,7 +52,7 @@ void log_warn(const char* filename, int32_t linenumber, const char* format, ...)
     va_start(args, format);
 
     char message[8192];
-    log_message(LOG_ANSI_COLOR_BLUE, "[WARN] ", filename, linenumber, format, 8192, message);
+    log_message(LOG_ANSI_BLUE, "[WARN] ", filename, linenumber, format, 8192, message);
 
     log_printf(message, args);
     va_end(args);
@@ -63,7 +63,7 @@ void log_fail(const char* filename, int32_t linenumber, const char* format, ...)
     va_start(args, format);
 
     char message[8192];
-    log_message(LOG_ANSI_COLOR_RED, "[FAIL] ", filename, linenumber, format, 8192, message);
+    log_message(LOG_ANSI_RED, "[FAIL] ", filename, linenumber, format, 8192, message);
 
     log_printf(message, args);
     va_end(args);
@@ -80,11 +80,11 @@ bool _log_assert(const char* condition, const char* format, ...) {
     va_list args;
     va_start(args, format);
 
-    printf("\n" LOG_ANSI_COLOR_RED "[FAIL]" LOG_ANSI_COLOR_RESET " assert( %s );\n", condition);
+    printf("\n" LOG_ANSI_RED "[FAIL]" LOG_ANSI_RESET " assert( %s );\n", condition);
     if( strlen(format) ) {
-        printf(LOG_ANSI_COLOR_BLUE);
+        printf(LOG_ANSI_BLUE);
         log_printf(format, args);
-        printf(LOG_ANSI_COLOR_RESET);
+        printf(LOG_ANSI_RESET);
         fflush(stdout);
     }
 
--- driver_log.h
+++ driver_log.h
@@ -27,13 +27,13 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-#define LOG_ANSI_COLOR_RED     "\x1b[31m"
-#define LOG_ANSI_COLOR_GREEN   "\x1b[32m"
-#define LOG_ANSI_COLOR_YELLOW  "\x1b[33m"
-#define LOG_ANSI_COLOR_BLUE    "\x1b[34m"
-#define LOG_ANSI_COLOR_MAGENTA "\x1b[35m"
-#define LOG_ANSI_COLOR_CYAN    "\x1b[36m"
-#define LOG_ANSI_COLOR_RESET   "\x1b[0m"
+#define LOG_ANSI_RED     "\x1b[31m"
+#define LOG_ANSI_GREEN   "\x1b[32m"
+#define LOG_ANSI_YELLOW  "\x1b[33m"
+#define LOG_ANSI_BLUE    "\x1b[34m"
+#define LOG_ANSI_MAGENTA "\x1b[35m"
+#define LOG_ANSI_CYAN    "\x1b[36m"
+#define LOG_ANSI_RESET   "\x1b[0m"
 
 #define log_stringify(a) #a
 #define log_tostring(x) log_stringify(x)
